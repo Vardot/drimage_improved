4a95ebd621af45e890c4378d69ffbd66415516ba
jose <j.bajawi@vardot.com>
2024-06-13 15:12:50 +0300

    Issue #3452971 by PrabuEla, Qusai Taha, josebc, Rajab Natshah: Redirect to 404 Page for Invalid Image Paths

--- src/EventSubscriber/DrimageSubscriber.php
+++ src/EventSubscriber/DrimageSubscriber.php
@@ -7,12 +7,14 @@ namespace Drupal\drimage_improved\EventSubscriber;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\Core\StreamWrapper\StreamWrapperManagerInterface;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
@@ -49,6 +51,13 @@ final class DrimageSubscriber implements EventSubscriberInterface {
   protected $configFactory;
 
   /**
+   * Logger Factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactory
+   */
+  protected $loggerFactory;
+
+  /**
    * Constructs a new DrimageRoutes object.
    *
    * @param \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface $stream_wrapper_manager
@@ -59,12 +68,15 @@ final class DrimageSubscriber implements EventSubscriberInterface {
    *   The module handler.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \Drupal\Core\Logger\LoggerChannelFactory $logger_factory
+   *   The logger factory.
    */
-  public function __construct(StreamWrapperManagerInterface $stream_wrapper_manager, EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler, ConfigFactoryInterface $config_factory) {
+  public function __construct(StreamWrapperManagerInterface $stream_wrapper_manager, EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler, ConfigFactoryInterface $config_factory, LoggerChannelFactoryInterface $logger_factory) {
     $this->streamWrapperManager = $stream_wrapper_manager;
     $this->entityTypeManager = $entity_type_manager;
     $this->moduleHandler = $module_handler;
     $this->configFactory = $config_factory;
+    $this->loggerFactory = $logger_factory->get('drimage_improved');
   }
 
   /**
@@ -75,7 +87,8 @@ final class DrimageSubscriber implements EventSubscriberInterface {
       $container->get('stream_wrapper_manager'),
       $container->get('entity_type.manager'),
       $container->get('module_handler'),
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('logger.factory')
     );
   }
 
@@ -85,7 +98,7 @@ final class DrimageSubscriber implements EventSubscriberInterface {
    * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
    *  The request event.
    */
-  public function onKernelRequest(RequestEvent $event): void {
+  public function onKernelRequest(RequestEvent $event) {
     // Check if the request contains with /styles/drimage_improved_.
     $path = $event->getRequest()->getPathInfo();
     if (strpos($path, '/styles/drimage_improved_')) {
@@ -127,6 +140,28 @@ final class DrimageSubscriber implements EventSubscriberInterface {
       $filename = end($parts);
       $filename_parts = explode('.', $filename);
       $format = end($filename_parts);
+
+      // Check if the image was found.
+      if (empty($image)) {
+        // Log an error message if the image is not found.
+        $this->loggerFactory->error('Image not found: @uri', ['@uri' => $scheme . '://' . urldecode($file_name)]);
+
+        // Throw a 404 error.
+        return new Response('Error generating image, missing source file.', 404);
+      }
+
+      // Get the first (and presumably only) image entity.
+      $images = reset($image);
+
+      // Check if the image entity is valid and has a valid ID.
+      if (!$images || !$images->id()) {
+        // Log an error message if the fid is not valid.
+        $this->loggerFactory->error('Invalid file ID for image: @uri', ['@uri' => $scheme . '://' . urldecode($file_name)]);
+
+        // Throw a 404 error.
+        return new Response('Error generating image, missing source file.', 404);
+      }
+
       // Create url from drimage_improved.image route.
       $url = Url::fromRoute('drimage_improved.image', [
         'width' => $width,
